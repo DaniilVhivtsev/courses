b9e749ee2aee58b3014efa6bb159b79e269a385c
dvshivtsev <dvshivtsev@naumen.ru>
2024-03-07 22:35:57 +0500

    Изменение базы данных на PostgreSQL.

--- src/main/java/com/fitness/courses/http/coach/course/model/entity/CourseCategoryEnum.java
+++ src/main/java/com/fitness/courses/http/coach/course/model/entity/CourseCategoryEnum.java
@@ -1,5 +1,37 @@
 package com.fitness.courses.http.coach.course.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum CourseCategoryEnum
 {
+    CARDIO("CARDIO");
+
+    private final String value;
+
+    CourseCategoryEnum(String value)
+    {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static CourseCategoryEnum fromValue(String value) {
+        for (CourseCategoryEnum b : CourseCategoryEnum.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected value '" + value + "'");
+    }
 }
